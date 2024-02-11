d762086fb32536003d7da762a7b7741759d6a149
knblomquist00 <blo18004@byui.edu>
2024-02-10 21:53:23 -0700

    dark mode

---
+++ chamber/scripts/darkMode.js
@@ -0,0 +1,26 @@
+
+const main = document.querySelector("body");
+const sections = document.querySelectorAll('section');
+const checkbox = document.querySelector(".switch input[type='checkbox']");
+
+function handleSwitch() {
+    const isChecked = checkbox.checked;
+
+    if (isChecked) {
+        main.style.background = "#000";
+        main.style.color = "#fff";
+        sections.forEach(section => {
+            section.classList.toggle('darkMode');
+        });
+    } else {
+        main.style.background = "#eee";
+        main.style.color = "#000";
+        
+        sections.forEach(section => {
+            section.classList.toggle('darkMode');
+        });
+    }
+}
+
+// Adding event listener to the checkbox
+checkbox.addEventListener('change', handleSwitch);
--- chamber/scripts/hamburgerbutton.js
+++ chamber/scripts/hamburgerbutton.js
@@ -1,7 +1,8 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
-
+const joinButton = document.querySelector('.join');
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
+	joinButton.classList.toggle('openJoin');
 });

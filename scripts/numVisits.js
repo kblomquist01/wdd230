8c3c1f3b15ac259503b1e338c4244d7f27943c6d
knblomquist00 <blo18004@byui.edu>
2024-02-22 15:14:15 -0700

    fix

--- scripts/numVisits.js
+++ scripts/numVisits.js
@@ -1,13 +1,13 @@
 const visitsDisplay = document.querySelector(".numvisits");
 
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+numVisits++;
 
-if (numVisits !== 0) {
+if (numVisits !== 1) {
 	visitsDisplay.textContent = numVisits;
 } else {
 	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
-    numVistits = 1;
 }
 
-numVisits++;
+
 localStorage.setItem("numVisits-ls", numVisits);

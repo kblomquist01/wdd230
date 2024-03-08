0fad1698f0b78b27b2d65d2117323bd5f81ece5d
knblomquist00 <blo18004@byui.edu>
2024-03-07 22:34:13 -0700

    save

---
+++ scripts/links.js
@@ -0,0 +1,28 @@
+const baseURL = "https://kblomquist01.github.io/wdd230/";
+const linksURL = "https://kblomquist01.github.io/wdd230/data/links.json";
+
+const cards = document.querySelector('#cards');
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+  }
+
+  const displayLinks = (weeks) => {
+        weeks.forEach((week) => {
+
+            let lesson = document.createElement('li');
+            lesson.innerHTML = `${week.lesson} `
+
+            week.links.forEach((link) => {
+                lesson.innerHTML += `<a href=${link.url}> ${link.title}</a>`
+            })
+            
+            cards.appendChild(lesson);
+        })
+        
+  }
+
+  
+getLinks();
